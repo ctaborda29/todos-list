89722c6d5d9a4b55f560054e36f31ab1d34463eb
ctaborda29 <ctaborda29@gmail.com>
2024-12-11 14:09:24 -0800

    todo CRUD using DB

---
+++ bdPostgres.js
@@ -0,0 +1,8 @@
+const {Pool} = require('pg');
+const pool = new Pool({
+
+
+})
+console.log(pool);
+
+module.exports = pool;
--- server.js
+++ server.js
@@ -1,38 +1,94 @@
 const express = require("express");
+const { sequelize } = require("./sequelize");
+//const pool = require("./bdPostgres");
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+async function fullBD() {
+  fetch(`https://jsonplaceholder.typicode.com/todos`)
+    .then(response => response.json())
+    .then(response =>{
+      response.forEach(async element => {
+        const { title, completed, id } = element;
+        const findtodo = await sequelize.findOne({ where: { id } });
+        if(findtodo){
+        await sequelize.create({
+          title,
+          completed,
+        });
+      }
+    });
+    })};
 
-let listToDo = [];
-
- fetch(`https://jsonplaceholder.typicode.com/todos`)
-.then(response => response.json()).then(response =>{ listToDo = response});
-
-
-
-
-
-app.get("/", (req, res) => {
-  res.json(listToDo);
+app.get("/fullBD", async (req, res) => {
+  const todos = await sequelize.findAll();
+  if (todos.length < 200) {
+    fullBD();
+    res.json({ message: "BD entered " });
+  } else res.json({ message: "BB full" });
 });
 
-app.post("/", (req, res) => {
-  const toDoBody = req.body;
-  const newToDo = { id: ++listToDo.length,...toDoBody}
-  listToDo.push(newToDo);
-  res.status(201).json(newToDo);
-
+app.get("/", async (req, res) => {
+  const todos = await sequelize.findAll();
+  res.json(todos);
 });
 
-app.put("/:id", (req, res) => {
-    const newData = req.body;
-   const id = Number(req.params.id);
-   const element = listToDo.find(item => item.id === id);
-   res.send(element);
-})
 
+app.get("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  try{
+    const findtodo = await sequelize.findOne({ where: { id } });
+    res.json(findtodo);
+  }
+  catch(err){
+    console.error(err)
+  }
+});
+
+app.post("/", async (req, res) => {
+  const { title, completed } = req.body;
+  const newTodo = await sequelize.create({
+    title,
+    completed,
+  });
+  res.status(201).json(newTodo);
+});
+
+app.put('/:id', async (req, res) => {
+  const { title, completed } = req.body;
+  const id = parseInt(req.params.id);
+    const findtodo = await sequelize.findOne({ where: { id } });
+    if (findtodo){
+      await sequelize.update(
+        { title, completed }, 
+        {
+          where: { id },
+        }
+      );
+      res.status(200).json(findtodo);
+      }
+else{
+  res.status(404).json({ message: "User not found" });
+}
+});
+
+app.delete("/:id", async (req,res)=>{
+  const id = Number(req.params.id);
+    const findtodo = await sequelize.findOne({ where: { id } });
+    if (findtodo){
+      const deleted = await sequelize.destroy({
+        where: { id },
+      });
+      console.log(`${deleted}: todos deleted`)
+      res.status(200).json(findtodo);
+      }
+else{
+  res.status(404).json({ message: "User not found" });
+}
+});
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
